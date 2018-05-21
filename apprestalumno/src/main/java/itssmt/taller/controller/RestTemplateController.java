7846ff7f2e8a6eeeb5a9a5207e388dbe59ba4de0
arturoquiroz <tutos4al@gmail.com>
2018-05-21 16:17:30 -0500

    Se creo este archivo que permite consumir servicios de otras aplicaciones.

---
+++ apprestalumno/src/main/java/itssmt/taller/controller/RestTemplateController.java
@@ -0,0 +1,30 @@
+package itssmt.taller.controller;
+
+import java.util.Arrays;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+@RequestMapping("/template")
+public class RestTemplateController {
+
+	RestTemplate restTemplate = new RestTemplate(); 
+	
+	//@RequestMapping(value="/products", method = RequestMethod.GET)
+	@GetMapping(value="/products")
+	public String getProductsByTemplate() {
+		System.out.println("Encontro el metodo");
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		return restTemplate.exchange("http://192.168.1.71:8090/cliente/consultatodos", HttpMethod.GET, entity, String.class).getBody();
+	}
+}
