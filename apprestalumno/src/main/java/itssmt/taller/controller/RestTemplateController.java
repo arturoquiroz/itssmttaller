68c4939bab3625063bc3a8225a3b868344ab76c9
arturoquiroz <tutos4al@gmail.com>
2018-05-22 13:41:25 -0500

    Metodo para consulta de estado de factura en servicio remoto agregado

--- apprestalumno/src/main/java/itssmt/taller/controller/RestTemplateController.java
+++ apprestalumno/src/main/java/itssmt/taller/controller/RestTemplateController.java
@@ -27,4 +27,14 @@ public class RestTemplateController {
 		HttpEntity<String> entity = new HttpEntity<String>(headers);
 		return restTemplate.exchange("http://192.168.1.71:8090/cliente/consultatodos", HttpMethod.GET, entity, String.class).getBody();
 	}
+	/* 
+	 * Metodo creado para consultar mediante el id o folio, el estado de un pedido y obtener su valor en
+	 * el body de la respuesta*/
+	@GetMapping(value="/factura")
+	public String getEstadoDeFactura(int id) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		return restTemplate.exchange("http://192.168.1.71:8090/estadodefactura?id="+id, HttpMethod.GET, entity, String.class).getBody();
+	}
 }
